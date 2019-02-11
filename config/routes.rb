a3db6ea7675c33be0df584592431f384a18ea941
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-02-11 09:34:23 -0700

    Remove root_path for auth lessons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'songs#index'
-
   resources :artists, only: [:index, :show, :new, :create] do
     resources :songs, only: [:index, :new, :create, :edit, :update]
   end
