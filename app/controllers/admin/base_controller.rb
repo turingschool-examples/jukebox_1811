4d85cf08ea9b3332c20a69dd2d8a03bd0018adea
ian douglas <ian.douglas@iandouglas.com>
2019-02-11 14:35:43 -0700

    final commit for authZ class

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,5 @@
+class Admin::BaseController < ApplicationController
+  before_action :require_admin
+
+  # here
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_admin
+    render file: '/public/404' unless current_admin?
+  end
 end
